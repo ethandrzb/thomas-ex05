736f5a30a04d85a8ae70ed4a2874b391f9fa6b3d
ethandrzb <ethandrzb@gmail.com>
2021-09-05 13:54:02 -0400

    Added baseline solution

---
+++ exercise05/src/main/java/Solution05.java
@@ -0,0 +1,43 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 5 Solution
+ *  Copyright 2021 Ethan Thomas
+ */
+
+import java.util.Scanner;
+
+// Pseudocode Solution
+
+// Prompt user for first number
+// Attempt to store user input in <num1>
+
+// Prompt user for second number
+// Attempt to store user input in <num2>
+
+// Compute sum of <num1> and <num2>
+// Compute difference of <num1> and <num2>
+// Compute product of <num1> and <num2>
+// Compute quotient of <num1> and <num2>
+// Print sum, difference, product, and quotient
+
+public class Solution05
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int num1;
+        int num2;
+
+        System.out.print("Enter the first number: ");
+        num1 = Integer.parseInt(sc.nextLine());
+
+        System.out.print("Enter the second number: ");
+        num2 = Integer.parseInt(sc.nextLine());
+
+        System.out.print(num1 + " + " + num2 + " = " + (num1 + num2) + '\n'
+                + num1 + " - " + num2 + " = " + (num1 - num2) + '\n'
+                + num1 + " * " + num2 + " = " + (num1 * num2) + '\n'
+                + num1 + " / " + num2 + " = " + (num1 / num2) + '\n');
+
+        System.exit(0);
+    }
+}
